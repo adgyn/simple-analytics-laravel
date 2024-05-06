49e96465f23c8facbcb1d951e91b26e298225422
Wanderson Costa <wcostaprijo@hotmail.com>
2024-05-06 13:17:15 -0300

    fix: migration name

--- src/SimpleAnalyticsProvider.php
+++ src/SimpleAnalyticsProvider.php
@@ -2,6 +2,8 @@
 
 namespace Adgyn\SimpleAnalytics;
 
+use Generator;
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider;
 
 class SimpleAnalyticsProvider extends ServiceProvider
@@ -15,8 +17,13 @@ class SimpleAnalyticsProvider extends ServiceProvider
             __DIR__.'/../config/analytics.php' => config_path('analytics.php'),
         ], 'simple-analytics');
 
-        $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], 'simple-analytics');
+        $migrations = [];
+        foreach (glob(__DIR__.'/../database/migrations/*.php') as $migration) {
+            $migrations[__DIR__.'/../database/migrations/'.$migration] = database_path('migrations/').now()->format('Y_m_d_His_').$migration;
+        }
+
+        if(!empty($migrations)) {
+            $this->publishesMigrations($migrations, 'simple-analytics');
+        }
     }
 }
